a74256d59cbe1abdf8881b94dcfad04ff24858af
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2023-01-21 12:16:28 -0300

    animating enter/exit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { render } from "react-dom";
-
+import React, { useState, useEffect, ReactNode } from "react";
 import "./styles.css";
 
 function useDelayUnmount(isMounted: boolean, delayTime: number) {
@@ -18,25 +16,64 @@ function useDelayUnmount(isMounted: boolean, delayTime: number) {
   return shouldRender;
 }
 
+// prettier-ignore
+interface AnimateRenderProps { isMounted: boolean; enter:string; exit: string; children: ReactNode }
+const AnimateRender: React.FC<AnimateRenderProps> = ({
+  isMounted,
+  children,
+  enter,
+  exit,
+}: AnimateRenderProps) => {
+  const shouldRenderChild = useDelayUnmount(isMounted, 500);
+  const mountedStyle = { animation: enter };
+  const unmountedStyle = { animation: exit };
+
+  if (!shouldRenderChild) return null;
+  return (
+    <div style={isMounted ? mountedStyle : unmountedStyle}>{children}</div>
+  );
+};
+
 const App: React.FC = () => {
   const [isMounted, setIsMounted] = useState(true);
-  const shouldRenderChild = useDelayUnmount(isMounted, 500);
-  const mountedStyle = { animation: "inAnimation 500ms ease-in" };
-  const unmountedStyle = { animation: "outAnimation 510ms ease-in" };
+  const [isMounted2, setIsMounted2] = useState(true);
+  const [isMounted3, setIsMounted3] = useState(true);
 
   const handleToggleClicked = () => {
     setIsMounted(!isMounted);
+    setTimeout(() => setIsMounted2(!isMounted2), 1000);
+    setTimeout(() => setIsMounted3(!isMounted3), 2000);
   };
 
   return (
     <main>
       <button onClick={handleToggleClicked}>Click me!</button>
-      {shouldRenderChild && (
-        <h1 style={isMounted ? mountedStyle : unmountedStyle}>YO DUDE!!!</h1>
-      )}
+
+      <AnimateRender
+        isMounted={isMounted}
+        enter="bounceIn 500ms ease-in"
+        exit="bounceOut 600ms ease-in"
+      >
+        <h1>YO DUDE!!!</h1>
+      </AnimateRender>
+
+      <AnimateRender
+        isMounted={isMounted2}
+        enter="fadeIn 500ms ease-in"
+        exit="fadeOut 600ms ease-in"
+      >
+        <h1>Hey buddy!!!</h1>
+      </AnimateRender>
+
+      <AnimateRender
+        isMounted={isMounted3}
+        enter="slideIn 500ms ease-in"
+        exit="slideOut 600ms ease-in"
+      >
+        <h1>Holla papi!!!</h1>
+      </AnimateRender>
     </main>
   );
 };
-
 
 export default App;
